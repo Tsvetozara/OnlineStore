8c205cce04e574b449186db418d212d346027f7f
Tsvetozara Tosheva <TsvetozaraTosheva@coherentsolutions.com>
2021-11-17 15:44:06 +0200

    MasterProduct and ProductVariation classes

---
+++ store/src/main/java/app/masterProduct/MasterProduct.java
@@ -0,0 +1,7 @@
+package app.masterProduct;
+
+public class MasterProduct {
+    private String name;
+    private Double rate;
+    private String description;
+}
---
+++ store/src/main/java/app/masterProduct/ProductVariation.java
@@ -0,0 +1,13 @@
+package app.masterProduct;
+
+public class ProductVariation {
+    private int quantity;
+    private int memory;
+    private String processor;
+    private double weight;
+    private String color;
+    private double displaySize;
+    private double displayResolution;
+    private double price;
+    private String batteryType;
+}
