3c59d2fc7eafac03bce5116b6945568f54134b75
Tsvetozara Tosheva <TsvetozaraTosheva@coherentsolutions.com>
2022-02-17 10:39:45 +0200

    Return getters and setters

--- store/src/main/java/com/coherentsolutions/trainings/java/auto/store/Category.java
+++ store/src/main/java/com/coherentsolutions/trainings/java/auto/store/Category.java
@@ -7,7 +7,25 @@ import java.util.List;
 public class Category {
     private String name;
 
+    public List<Category> categories;
+
     public List<MasterProduct> products;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
 
     public Category(String name) {
         this.name = name;
@@ -16,4 +34,9 @@ public class Category {
     public void setProducts(List<MasterProduct> products) {
         this.products = products;
     }
+
+    public List<MasterProduct> getProducts() {
+        return products;
+    }
 }
+
--- store/src/main/java/com/coherentsolutions/trainings/java/auto/store/productpackage/ProductVariation.java
+++ store/src/main/java/com/coherentsolutions/trainings/java/auto/store/productpackage/ProductVariation.java
@@ -11,8 +11,82 @@ public class ProductVariation {
     private double price;
     private String batteryType;
 
+    public ProductVariation(int quantity, int memory, String processor, double weight, String color,
+                            double displaySize, double displayResolution, double price,
+                            String batteryType, String name, Double rate, String description) {
+        this.quantity = quantity;
+        this.memory = memory;
+        this.processor = processor;
+        this.weight = weight;
+        this.color = color;
+        this.displaySize = displaySize;
+        this.displayResolution = displayResolution;
+        this.price = price;
+        this.batteryType = batteryType;
+    }
+
     public ProductVariation(String color, double price) {
         this.color = color;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int quantity) {
+        this.memory = memory;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setDisplaySize(double displaySize) {
+        this.displaySize = displaySize;
+    }
+
+    public double getDisplaySize() {
+        return displaySize;
+    }
+
+    public void setDisplayResolution(double displayResolution) {
+        this.displayResolution = displayResolution;
+    }
+
+    public double getDisplayResolution() {
+        return displayResolution;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
@@ -20,4 +94,13 @@ public class ProductVariation {
         return price;
     }
 
+    public void setBatteryType(String batteryType) {
+        this.batteryType = batteryType;
+    }
+
+    public String getBatteryType() {
+        return batteryType;
+    }
+
 }
+
